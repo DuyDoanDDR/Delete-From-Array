1aa6fd013248b9217e2c6a795d137717795dc39d
Doan Minh Duy <duydoan812@gmail.com>
2025-04-25 01:33:02 +0700

    Finish_Commit

---
+++ Delete From Array_VS/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] Value = new int[] { 10, 4, 6, 7, 8, 6, 0, 0 };
+int n = Value.Length;
+
+Console.WriteLine("Nhap phan tu can xoa: ");
+
+if (int.TryParse(Console.ReadLine(), out int target) && target >= 0)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (target == Value[i])
+        {
+            for (int j = i; j < n - 1; j++)
+            {
+                Value[j] = Value[j + 1];
+            }
+            Value[n - 1] = 0;
+
+            break;
+        }
+    }
+}
+
+Console.WriteLine("Mang moi: ");
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(Value[i] + "");
+}
